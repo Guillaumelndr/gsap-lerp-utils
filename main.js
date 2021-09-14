a3c2ca49cac90d0a056351ce954636cc3bed1750
Guillaume Landré <guillaumelandre101@gmail.com>
2021-09-14 20:31:04 +0200

    test requestAnimationFrame

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 import gsap from 'gsap'
 
-console.log(gsap)
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+
+const btn = document.querySelector('button')
+const target = document.querySelector('p')
+
+let start = 0
+const end = 200
+const amt = 0.01
+
+const render = () => {
+    start = gsap.utils.interpolate(start, end, amt)
+    gsap.set(target, {x: start})
+    if (start < end)
+        requestAnimationFrame(render)
+}
+
+const startAnimation = e => {
+    requestAnimationFrame(render)
+}
+
+
+
+btn.addEventListener('click', startAnimation)
